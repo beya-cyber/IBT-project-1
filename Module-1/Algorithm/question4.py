965d584882f4c4ecf56574ce4a98a64977ecbef8
beya-cyber <tazabitech@gmail.com>
2026-07-24 22:00:03 +0300

    This is module 1 answer

--- Module-1/Algorithm/question4.py
+++ Module-1/Algorithm/question4.py
@@ -5,10 +5,19 @@ def checkMeera(lst):
 
     for num in lst:
         if num == 0:
-            if is_meera:
-                print("I am a Meera array")
-            else:
-                print("I am NOT a Meera array")
+            # If 0 is in the array, 0 * 2 = 0.
+            # It only breaks the rule if 0 appears more than once.
+            if lst.count(0) > 1:
+                is_meera = False
+                break
+        elif (num * 2) in num_set:
+            is_meera = False
+            break
+
+    if is_meera:
+        print("I am a Meera array")
+    else:
+        print("I am NOT a Meera array")
 
 checkMeera([7, 4, 9])
 checkMeera([1, -6, 4, -3])
